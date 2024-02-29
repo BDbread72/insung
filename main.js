b8a903591eb240b2f04354e3e080a392cf60b48c
BDbread72 <53614828+BDbread72@users.noreply.github.com>
2024-02-29 13:27:39 +0900

    INSUNG MAP - SPYGLASS (level 1)
    SPYGLASS (Can you see Us?) 가 추가되었습니다.

---
+++ main.js
@@ -0,0 +1,32 @@
+const salt = "sakgoon";
+const encrypt = (text) => {if(!text) return''; return CryptoJS.AES.encrypt(text,salt).toString()}
+const decrypt = (text) => {if(!text) return''; try{const bytes=CryptoJS.AES.decrypt(text,salt);return bytes.toString(CryptoJS.enc.Utf8)}catch(error){console.log(error);return '';}}
+var bodyPad = {
+  SPYGLASS:{
+    password:"c11103d0027b062d759e79ed790ffa43",
+    story:"당신은 이제 막 잠들었습니다. 지금 당장 떠오르는 것은 어제 틀린 코딩문제에 대한 해결책 따위죠. 당신은 매일 그러한 것 처럼, 악몽에 빠집니다.",
+    link:"U2FsdGVkX180skaHk0oFposgIECs5xH5oIQuRVJ/Ht7+oHsCyEz/luW6zwsA2uy6NovU8Xw6QUfrclERcXCtBD98t7vdegbCiH8xXAHjc0KcIjGu8TZnYmKiCiWWkFvx52xPFgXU9aPak3Dqz3T1tg==",
+  },
+  TOOLKID:{
+    password:"637d8697621a53b596a9c47b6a410df1",
+    story:'당신은 하나의 악몽에서 깨어났다고 생각 했습니다. 그러나, 다음 악몽에 잠들기 까지는 오래 걸리지 않았죠.\n\nWIP(Work In Progress)',
+    link:"U2FsdGVkX18NLZxky8hyd/fLmBIsp++Ee/bEfsGQMvutrEYB4+FAJqbDHXD5VQFK"
+  }
+}
+
+function confirm() {
+  const title = document.getElementById("title").value.toUpperCase()
+  const password = document.getElementById("password").value.toUpperCase()
+
+  let pass=false
+  if(bodyPad[title]){
+    if(bodyPad[title].password == undefined) pass=true
+    else if(bodyPad[title].password == CryptoJS.MD5(password).toString()) pass=true;
+  }
+
+  if(pass){
+    document.getElementById("story").innerText=bodyPad[title].story
+    document.getElementById("link").href = decrypt(bodyPad[title].link)
+    document.getElementById("link").innerText = decrypt(bodyPad[title].link)
+  }
+}
